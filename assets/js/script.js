b2088630d8081afc62344e0b32df13da27c53ec1
Hampus Nilsson <hampusjojo@gmail.com>
2021-08-28 18:25:15 +0000

    format code and clean up script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,76 @@
+let time = 950;
+var startTime;
+var delay;
+
 /**
  * Theese nested functions adds active class and removes target class.
  * And on timeout it removes active class and adds target class again.
  */
-let time = 950
-var startTime;
- function unMask() {
-    let targetActive = document.getElementsByClassName("target")[Math.floor(Math.random()*40)]
-    if(targetActive.classList !== "active"){
-        targetActive.classList.add("active")
-        targetActive.classList.remove("target")
+function unMask() {
+    let targetActive = document.getElementsByClassName("target")[Math.floor(Math.random() * 40)];
+    if (targetActive.classList !== "active") {
+        targetActive.classList.add("active");
+        targetActive.classList.remove("target");
         targetActive.addEventListener('click', onHit);
-        if (Date.now() - startTime > 60000){
+        if (Date.now() - startTime > 60000) {
             stopGame();
+        }
+        if (1) {
+            return setTimeout(mask, time);
+        }
+    }
+
+    function mask() {
+        if (targetActive.classList !== "target") {
+            targetActive.classList.remove("active");
+            targetActive.classList.add("target");
+            targetActive.removeEventListener('click', onHit);
+        }
+    }
 }
-if (1){
-    return setTimeout(mask, time);
-}
-}
-function mask (){
-    if(targetActive.classList !== "target"){
-    targetActive.classList.remove("active")
-    targetActive.classList.add("target")
-    targetActive.removeEventListener('click', onHit);
-}}}
+
 /**
  * Sets interval on unMask() to start the game.
  */
- function startUnMask(func, interval) {
+function startUnMask(func, interval) {
     func();
-    globalThis.delay = setInterval(func, interval);
+    delay = setInterval(func, interval);
     startTime = Date.now();
 }
+
 /**
  * Is called upon clicking start button
  * Calls startUnMask()
  */
 function startSetInterval() {
     startUnMask(unMask, 1000);
-    document.getElementById("score").innerHTML = 0
+    document.getElementById("score").innerHTML = 0;
 }
+
 /**
  * Function to clearinterval and stop game from running.
-*/
-function stopGame(){
-    clearInterval(globalThis.delay)
-    clearTimeout
+ */
+function stopGame() {
+    clearInterval(delay);
 }
+
 /**
  * Removes active class and makes it invisble again. And calls function,
  * scoreIncrement().
  * Also takes time off the timeout to increase difficulty on target hit. 
  */
- function onHit (){
-    this.classList.remove("active")
-    this.classList.add("target")
+function onHit() {
+    this.classList.remove("active");
+    this.classList.add("target");
     scoreIncrement();
-    time = time-10
+    time = time - 10;
 }
+
 /**
  * Takes old score and adds 1 for each time onHit() is called.
  * Borrowed from love-maths project Coding Institute
  */
 function scoreIncrement() {
-    let oldscore = parseInt(document.getElementById("score").innerHTML)
+    let oldscore = parseInt(document.getElementById("score").innerHTML);
     document.getElementById("score").innerHTML = ++oldscore;
 }
-
-
