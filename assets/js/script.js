a91f5293e121cc9100a3183057d7a177cc311235
Hampus Nilsson <hampusjojo@gmail.com>
2021-08-28 03:04:08 +0000

    add onHit function and scoreIncrement

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,24 @@ function mask (){
     targetActive.classList.add("target")
     targetActive.removeEventListener('click', onHit);
 }}}
-
+/**
+ * Removes active class and makes it invisble again. And calls function,
+ * scoreIncrement().
+ * Also takes time off the timeout to increase difficulty on target hit. 
+ */
+ function onHit (){
+    this.classList.remove("active")
+    this.classList.add("target")
+    scoreIncrement();
+    time = time-10
+}
+/**
+ * Takes old score and adds 1 for each time onHit() is called.
+ * Borrowed from love-maths project Coding Institute
+ */
+function scoreIncrement() {
+    let oldscore = parseInt(document.getElementById("score").innerHTML)
+    document.getElementById("score").innerHTML = ++oldscore;
+}
 
 
